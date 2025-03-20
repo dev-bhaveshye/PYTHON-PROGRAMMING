a76fff65000aedab33a110eb07de1fa445346263
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-20 21:08:29 +0530

    Add files via upload

---
+++ break.py
@@ -0,0 +1,12 @@
+for i in range (100):
+    if i==55:
+        break #exit loop
+    print(i)
+
+for i in range (100):
+    if i==55:
+        continue #skip 
+    print(i)
+
+for i in range (4):
+    pass # do nothing with this loop
