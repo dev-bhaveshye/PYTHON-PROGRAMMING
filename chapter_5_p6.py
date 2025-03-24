9681020c49e8553c477b7bbfd367584e2abab0be
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-24 08:33:38 +0530

    Add files via upload

---
+++ chapter_5_p6.py
@@ -0,0 +1,19 @@
+d={}
+
+name1=input("enter friend name ")
+lang=input("enter language ")
+d.update({name1:lang})
+
+name2=input("enter friend name ")
+lang=input("enter language ")
+d.update({name2:lang})
+
+name3=input("enter friend name ")
+lang=input("enter language ")
+d.update({name3:lang})
+
+name4=input("enter friend name ")
+lang=input("enter language ")
+d.update({name4:lang})
+
+print(d)
