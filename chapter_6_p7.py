0a224718592b5cfe1ff08470acb256ca82217186
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-24 22:55:02 +0530

    Add files via upload

---
+++ chapter_6_p7.py
@@ -0,0 +1,7 @@
+post = " helloo , bhavesh is a good boy , he has very much knoledge about coding"
+
+if ( "bhavesh".lower() in post.lower() ):
+    print("the post is talking about bhavesh ")
+
+else:
+    print(" post is not talking about bhavesh ")
