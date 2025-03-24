57c259a5c37010450bcc869864dd8f58969d17dc
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-24 22:54:01 +0530

    Add files via upload

---
+++ chapter_6_p4.py
@@ -0,0 +1,8 @@
+name = input("enter name :")
+len = len(name)
+
+if len <=10 :
+    print (" name contain less than 10 characters , name contains " , len , "characters")
+
+else:
+    print("name  contain more than 10 character , name contains ", len , "characters")
