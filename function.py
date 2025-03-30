f955c550dee3083fde14219132fbfcf1f371fe0e
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-30 23:14:03 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,17 @@
+def avg():
+    a=int(input("enter the number"))
+    b=int(input("enter the number"))
+    c=int(input("enter the number"))
+    
+    avg=( a + b + c)/3
+    print(avg)
+
+avg()
+
+
+def goodday():
+    a=input("enter your name :")
+    print(f"good day {a}")
+
+
+goodday()
