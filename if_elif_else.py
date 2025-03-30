7b79cc2752378ccd8a29dbb69c1dfae6206c62ed
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-30 23:14:41 +0530

    Add files via upload

---
+++ if_elif_else.py
@@ -0,0 +1,10 @@
+a=int(input("enter your age "))
+
+if(a>18):
+    print("you are eligible")
+
+elif(a==0):
+    print("you have entered zero which is not a valid age")
+
+else:
+    print("you are not eligible ")
