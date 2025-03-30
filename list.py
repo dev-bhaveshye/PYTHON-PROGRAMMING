9fcd1c09696b103d2721414744bea68de6056922
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-30 23:16:01 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,23 @@
+section = ["bhavesh","yash","darshan","tanmay"]
+section[3]="lokesh"
+print(section[3])
+print(section[0:4])
+
+(section.append("dipesh"))
+print(section)
+
+l1 = [1 ,4 ,7 ,2 ,3]
+l1.sort() # sorting
+print(l1)
+
+l1.reverse() #reverse 
+print(l1)
+
+l1.insert(3,9) #insert at index < ondex , object
+print(l1)
+
+l1.pop(3) #pop using index
+print(l1)
+
+l1.remove( 2) # remove direct object
+print(l1)
