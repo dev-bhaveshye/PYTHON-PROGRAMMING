d1fd765ccfc79398a5f7819df1b3e06fb5619232
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-31 23:18:40 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,17 @@
+b=(1,2,5,4,3,"bhavesh",True)
+print(type(b))
+
+no = b.count(4)
+print(no)
+
+c=b.index(3)
+print(c)
+
+print(2 in b) # search in tupple 
+
+print(b*3) #duplicate the tupple
+
+print(len(b)) #length of tuple 
+
+print(b[0:4])
+
