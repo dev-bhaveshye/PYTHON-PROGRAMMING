8b1433de67376294009045380604d2495087adeb
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-31 23:18:15 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,15 @@
+name = "bhavesh"
+nameshort = name[0:4] # 0,1,2,3 from starting , -1,-2,-3 from ending of string
+name1=name[1]
+print(name1)
+print(nameshort)
+
+
+b=name[-1:-5]
+print(b)
+
+numbers="12345678"
+numbers[1:7:2]
+print(numbers[1:7:2])
+numbers[0:8]
+print(numbers[0:8])
