12efd57caebfb1628f2a50c71b6bbfb1bde5f561
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-31 23:17:53 +0530

    Add files via upload

---
+++ str_function.py
@@ -0,0 +1,13 @@
+name="bhavesh"
+print(len(name))
+
+#starting ending
+print(name.endswith("esh"))
+print(name.endswith("by"))
+print(name.startswith("by"))
+
+#capital
+print(name.capitalize())
+
+sent = " bhavesh is a good boy"
+print(sent.replace("good","bad"))
